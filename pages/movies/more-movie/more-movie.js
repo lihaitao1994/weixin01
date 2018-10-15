8db81c88807f4b913d54239a52d4b6011268dc46
Mr.3 <574424364@qq.com>
2018-10-15 23:53:04 +0800

    电影详情页面3/2

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -44,6 +44,7 @@ Page({
         var refreshUrl = this.data.requestUrl + '?star=0&count=20';
         this.data.movies = {}
         this.data.isEmpty = true
+        this.data.totalCount = 0
         util.http(refreshUrl, this.processDoubanData)
         wx.stopPullDownRefresh()
         wx.showNavigationBarLoading()
--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -3,7 +3,7 @@ var app = getApp()
 Page({
 
     data: {
-
+        movie: {}
     },
 
     onLoad: function(options) {
@@ -13,7 +13,42 @@ Page({
     },
 
     processDoubanData: function(data) {
-        
+        if (!data) {
+            return
+        }
+        var director = {
+            avatar: "",
+            name: "",
+            id: ""
+        }
+        if (data.directors[0] != null) {
+            if (data.directors[0].avatars != null) {
+                director.avatar = data.directors[0].avatars.large
+
+            }
+            director.name = data.directors[0].name;
+            director.id = data.directors[0].id;
+        }
+        var movie = {
+            movieImg: data.images ? data.images.large : "",
+            country: data.countries[0],
+            title: data.title,
+            originalTitle: data.original_title,
+            wishCount: data.wish_count,
+            commentCount: data.comments_count,
+            year: data.year,
+            generes: data.genres.join("、"),
+            stars: util.convertToStarsArray(data.rating.stars),
+            score: data.rating.average,
+            director: director,
+            casts: util.convertToCastString(data.casts),
+            castsInfo: util.convertToCastInfos(data.casts),
+            summary: data.summary
+        }
+
+        this.setData({
+            movie: movie
+        })
     }
 
 })
